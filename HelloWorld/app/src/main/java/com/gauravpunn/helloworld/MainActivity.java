8a7beb3e3c930e1298b4cc1b5a2a11fde4794094
gaurav <gauravpunn39@hotmail.com>
2018-05-30 22:02:13 +0530

    Application Icon Changed and Toasts are created for each method of life cycle of Activity also Toast for onCreate method appears in Center

--- HelloWorld/app/src/main/java/com/gauravpunn/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/gauravpunn/helloworld/MainActivity.java
@@ -2,12 +2,55 @@ package com.gauravpunn.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_next);
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Activity Created",Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER|Gravity.CENTER,0,0);
+        toast.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Activity Started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Activity is on Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Resumed Activity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Activity Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Activity Restarted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Activity get Distroyed", Toast.LENGTH_SHORT).show();
     }
 }
